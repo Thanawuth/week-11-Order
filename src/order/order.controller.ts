a8c5231fbcc9070bf390a92d422da1acbeed9aec
Aftertnw <64160049@go.buu.ac.th>
2024-09-22 22:14:50 +0700

    add messagepattern

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -6,18 +6,32 @@ import {
   Patch,
   Param,
   Delete,
+  Inject,
 } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import {
+  ClientProxy,
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 
 @Controller('order')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+  constructor(
+    private readonly orderService: OrderService,
+    @Inject('INVENTORY_SERVICE') private inventoryService: ClientProxy,
+  ) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.orderService.create(createOrderDto);
+  async create(@Body() createOrderDto: CreateOrderDto) {
+    const newOrder = await this.orderService.create(createOrderDto);
+    console.log(' Order send to inventory for processing');
+    this.inventoryService.emit('order_created', newOrder);
+    return newOrder;
   }
 
   @Get()
@@ -38,5 +52,23 @@ export class OrderController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.orderService.remove(+id);
+  }
+
+  @MessagePattern('order_completed')
+  async handleOrderCompleted(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    data.status = 'completed';
+    await this.orderService.update(data.id, data);
+    channel.ack(originalMsg);
+  }
+
+  @MessagePattern('order_canceled')
+  async handleOrderCanceled(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    data.status = 'canceled';
+    await this.orderService.update(data.id, data);
+    channel.ack(originalMsg);
   }
 }
